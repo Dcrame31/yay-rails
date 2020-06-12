586f59ab2e16d61014495c08c47671c23febbd33
Diane Crame <dcrame@icloud.com>
2020-06-12 10:18:47 -0700

    changed models

--- app/controllers/gardens_controller.rb
+++
@@ -1,2 +0,0 @@
-class GardensController < ApplicationController
-end
--- db/migrate/20200612021224_create_plants.rb
+++ db/migrate/20200612021224_create_plants.rb
@@ -1,6 +1,6 @@
-class CreatePlants < ActiveRecord::Migration[6.0]
+class CreateBudgets < ActiveRecord::Migration[6.0]
   def change
-    create_table :plants do |t|
+    create_table :budgets do |t|
       t.string :name
       t.integer :category_id
     end
