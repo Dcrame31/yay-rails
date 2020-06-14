1567c44fbc8ae1a8f31e7cef82e517fbbd52a59d
Diane Crame <dcrame@icloud.com>
2020-06-13 17:26:03 -0700

    fixed access to login path when already logged in

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy", as: :logout
-  delete '/logout', to: "sessions#destroy"
+  
   
   #get 'auth/:provider/callback', to: 'sessions#create'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
