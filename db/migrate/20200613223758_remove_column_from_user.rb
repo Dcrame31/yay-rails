408f3aef5b6a3c696d251ade5a302d0b3fe9e57b
Diane Crame <dcrame@icloud.com>
2020-06-21 16:50:01 -0700

    added admin attribute to user

--- db/migrate/20200613223758_remove_column_from_user.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnFromUser < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :google_token, :string
-    remove_column :users, :google_refresh, :string
-    remove_column :users, :google_refresh_token, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_183252) do
+ActiveRecord::Schema.define(version: 2020_06_21_035810) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,10 +36,7 @@ ActiveRecord::Schema.define(version: 2020_06_18_183252) do
     t.integer "category_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'false' for column 'default'
 
 end
