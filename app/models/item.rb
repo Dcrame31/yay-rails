9d334041f67e7b5bb63e375cacf039f177931a51
Diane Crame <dcrame@icloud.com>
2020-06-20 18:17:58 -0700

    added custom validation and updated routes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,21 @@ class Item < ApplicationRecord
     validates :name, :presence => true
     validates :price, :presence => true
     accepts_nested_attributes_for :list
+    validate :zero?
 
     def item_cost
         if self.qty.to_i > 0
             self.qty.to_i * self.price.to_i
         else
             self.price.to_i
+        end
+    end
+
+    def zero?
+        if self.price.to_i < 0
+            errors.add(:price, "Price can't be less than 0.")
+        elsif self.price.to_i == 0
+            errors.add(:price, "Price can't be negative.")
         end
     end
 end
