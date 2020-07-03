a7b340ea11edff9a10cab023fbb0b4d11ffd15d0
Diane Crame <dcrame@icloud.com>
2020-07-03 10:19:52 -0700

    fixed omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_203607) do
+ActiveRecord::Schema.define(version: 2020_07_03_160659) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,7 +38,11 @@ ActiveRecord::Schema.define(version: 2020_06_22_203607) do
     t.integer "category_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'false' for column 'default'
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+  end
 
 end
